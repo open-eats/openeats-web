b8a5329f4c9c5cdd872c0d3651ff96ad424734dc
ryan.noelk <ryannoelk@gmail.com>
2018-02-10 12:59:52 -0500

    adding menus

---
+++ modules/menu/containers/Menu.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import Loading from '../../base/components/Loading'
+// import * as MenuActions from '../actions/MenuActions'
+// import * as MenuItemActions from '../actions/MenuItemActions'
+// import bindIndexToActionCreators from '../../common/bindIndexToActionCreators'
+import documentTitle from '../../common/documentTitle'
+
+class Menu extends React.Component {
+  componentDidMount() {
+    // this.props.recipeActions.load(this.props.match.params.recipe);
+  }
+
+  componentWillUnmount() {
+    documentTitle();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // if (nextProps.match.params.recipe !== this.props.match.params.recipe) {
+    //   nextProps.recipeItemActions.reset();
+    //   nextProps.recipeActions.load(nextProps.match.params.recipe);
+    //   window.scrollTo(0, 0);
+    // }
+  }
+
+  render() {
+    // let { lists, recipes, match, status, user } = this.props;
+    // let { recipeActions, recipeItemActions } = this.props;
+    // let data = recipes.find(t => t.id == match.params.recipe);
+    if (true) {
+      // let showEditLink = (user !== null && user.id === data.author);
+      // documentTitle(data.title);
+      return (
+          <div>hi</div>
+      );
+    } else {
+      return ( <Loading message="Loading"/> )
+    }
+  }
+}
+
+// Menu.propTypes = {
+//   recipes: PropTypes.array.isRequired,
+//   lists: PropTypes.array.isRequired,
+//   status: PropTypes.string.isRequired,
+//   user: PropTypes.object.isRequired,
+//   match: PropTypes.object.isRequired,
+//   recipeActions: PropTypes.object.isRequired,
+//   recipeItemActions: PropTypes.object.isRequired,
+// };
+
+const mapStateToProps = state => ({
+  // user: state.user,
+  // recipes: state.recipe.recipes,
+  // status: state.recipe.status,
+  // lists: state.list.lists,
+});
+
+const mapDispatchToProps = (dispatch, props) => ({
+  // recipeItemActions: bindActionCreators(MenuItemActions, dispatch),
+  // recipeActions: bindActionCreators(
+  //   bindIndexToActionCreators(MenuActions, props.match.params.recipe),
+  //   dispatch
+  // ),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Menu);
