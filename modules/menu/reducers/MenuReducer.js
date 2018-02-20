5ecaca8e1cfdbebbd49644c04f6cfce326f2b7d6
ryan.noelk <ryannoelk@gmail.com>
2018-02-20 14:56:29 -0500

    updating menus

--- modules/menu/reducers/MenuReducer.js
+++ modules/menu/reducers/MenuReducer.js
@@ -4,19 +4,19 @@ const items = (state = [], action) => {
   switch (action.type) {
     case MenuConstants.MENU_LOAD:
       return action.data;
-    // case MenuItemConstants.MENU_ITEM_ADD:
-    //   return [
-    //     ...state,
-    //     { ...action, completed: false }
-    //   ];
-    // case MenuItemConstants.MENU_ITEM_SAVE:
-    //   return state.map(item =>
-    //     item.id === action.id ?
-    //       { ...item, title: action.title } :
-    //       item
-    //   );
-    // case MenuItemConstants.MENU_ITEM_DELETE:
-    //   return state.filter(t => t.id !== action.id);
+    case MenuConstants.MENU_ADD:
+      return [
+        ...state,
+        { ...action, completed: false }
+      ];
+    case MenuConstants.MENU_SAVE:
+      return state.map(item =>
+        item.id === action.id ?
+          { ...item, title: action.title } :
+          item
+      );
+    case MenuConstants.MENU_DELETE:
+      return state.filter(t => t.id !== action.id);
     default:
       return state
   }
