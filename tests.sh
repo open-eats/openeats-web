0e55cd301afee254b70678de52a6a55109ab2062
ryan.noelk <ryannoelk@gmail.com>
2018-03-20 11:32:23 +0100

    test

--- tests.sh
+++ tests.sh
@@ -4,10 +4,9 @@
 yarn add coveralls
 apk update
 apk add git
-apk add curl
 
 # Run the tests and create a coverage report
 jest --coverage
 
 # Submit coverage to Coveralls
-coveralls
+./node_modules/coveralls/bin/coveralls.js
