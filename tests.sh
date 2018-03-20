de31d6dc2a27027ba9a525e8ca9843b5bc0347fe
ryan.noelk <ryannoelk@gmail.com>
2018-03-20 11:29:09 +0100

    removing code climate from tests

--- tests.sh
+++ tests.sh
@@ -6,16 +6,8 @@ apk update
 apk add git
 apk add curl
 
-# Install Code Climate script
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-./cc-test-reporter before-build
-
 # Run the tests and create a coverage report
 jest --coverage
 
 # Submit coverage to Coveralls
 coveralls
-
-# Submit report to Code Climate
-./cc-test-reporter after-build --coverage-input-type coverage.py --exit-code 0
