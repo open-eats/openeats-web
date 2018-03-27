124480fa6245889c0ec8f4a62c11b6e6b1bef4c4
ryan.noelk <ryannoelk@gmail.com>
2018-03-27 12:51:13 +0200

    checking passed tests now

--- tests.sh
+++ tests.sh
@@ -4,11 +4,9 @@
 yarn add coveralls@2.12.0
 
 yarn test -- --coverage
-if [ $? -eq 0 ]
-then
-  exit 0
-else
-  exit 1
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit 1
 fi
 
 # Submit coverage to Coveralls
