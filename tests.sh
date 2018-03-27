58e662f9437ebdca59e5090cea142ef31edd5461
ryan.noelk <ryannoelk@gmail.com>
2018-03-27 12:23:57 +0200

    fail tests if they fail

--- tests.sh
+++ tests.sh
@@ -3,24 +3,11 @@
 # Install test related dependencies
 yarn add coveralls@2.12.0
 
-# Run the tests and create a coverage report
-#yarn test
-#if [ $? -eq 0 ]
-#then
-#  echo "The script ran ok"
-#  exit 0
-#else
-#  echo "The script failed" >&2
-#  exit 1
-#fi
-
 yarn test -- --coverage
 if [ $? -eq 0 ]
 then
-  echo "The script ran ok"
   exit 0
 else
-  echo "The script failed!" >&2
   exit 1
 fi
 
