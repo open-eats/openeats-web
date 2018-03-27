0805ad89b090dfe19e19961491f081701b42d87a
ryan.noelk <ryannoelk@gmail.com>
2018-03-27 12:05:30 +0200

    adding if check to fail if build fails

--- tests.sh
+++ tests.sh
@@ -5,7 +5,24 @@ yarn add coveralls@2.12.0
 
 # Run the tests and create a coverage report
 yarn test
+if [ $? -eq 0 ]
+then
+  echo "The script ran ok"
+  exit 0
+else
+  echo "The script failed" >&2
+  exit 1
+fi
+
 yarn test -- --coverage
+if [ $? -eq 0 ]
+then
+  echo "The script ran ok"
+  exit 0
+else
+  echo "The script failed" >&2
+  exit 1
+fi
 
 # Submit coverage to Coveralls
 ./node_modules/coveralls/bin/coveralls.js < coverage/lcov.info
