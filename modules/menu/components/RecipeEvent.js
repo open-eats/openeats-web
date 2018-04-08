b1ba020867540448adb46f9ec67e5fc99bc803eb
ryan.noelk <ryannoelk@gmail.com>
2018-04-08 11:05:19 +0200

    adding recipe slug to the menu

--- modules/menu/components/RecipeEvent.js
+++ modules/menu/components/RecipeEvent.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const RecipeEvent = ({ event, title, isAllDay }) => (
   <div className="calender-recipe-event">
-    <Link to={ '/recipe/' + event.recipe }>
+    <Link to={ '/recipe/' + event.recipe_slug }>
       { event.recipe_title  }
     </Link>
     <span className="glyphicon glyphicon-pencil pull-right"/>
