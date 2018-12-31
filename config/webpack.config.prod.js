e9ec5a4335db4bd6c2f43f39a6419b513b2683bb
ryannoelk <ryannoelk@gmail.com>
2018-12-31 18:08:55 +0100

    fixing tests

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -324,10 +324,10 @@ module.exports = {
       },
       minify: true,
       // For unknown URLs, fallback to the index page
-      navigateFallback: publicUrl + '/index.html',
+      // navigateFallback: publicUrl + '/index.html',
       // Ignores URLs starting from /__ (useful for Firebase):
       // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      // navigateFallbackWhitelist: [/^(?!\/__).*/],
       // Don't precache sourcemaps (they're large) and build asset manifest:
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
     }),
