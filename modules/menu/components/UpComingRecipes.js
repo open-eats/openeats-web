ad63b84e65bfdb04d6220b1ad81dd483be510b8f
ryannoelk <ryannoelk@gmail.com>
2019-03-08 15:59:19 -0500

    code cleanup

--- modules/menu/components/UpComingRecipes.js
+++ modules/menu/components/UpComingRecipes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import OnTheMenu from './OnTheMenu'
-import Menu from "../../news/components/News";
 
 const FullMenu = ({ menuItems, completeMenuItem, editMenuItem }) => {
   let nextWeek = moment().add(1, 'week').startOf('week').format('MMMM D');
